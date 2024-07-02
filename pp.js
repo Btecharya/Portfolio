9a9a55fcfb746e1d62ee6bad8b75dcbb2c07710c
Sahani Ashish Kumar <90511449+Btecharya@users.noreply.github.com>
2024-07-02 20:56:23 +0530

    Add files via upload

---
+++ pp.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>
+            Portfolio
+        </title>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+        <header class="header">
+            <a href="#" class="logo">portfolio.</a>
+            <nav class="navbar">
+                <a href="#" style="--i:1" class="active">Home</a>
+                <a href="#" style="--i:2">About</a>
+                <a href="#" style="--i:3">Skills</a>
+                <a href="#" style="--i:4">Project</a>
+                <a href="#" style="--i:5">Contact</a>
+
+            </nav>
+        </header>
+    <section class="home">
+<div class="home-content">
+    <h3>Hello, It's Me</h3>
+    <h1>
+        Ashish Sahani
+    </h1>
+    <h3>And I'm a <span class="multiple-text"></span></h3>
+    <p> Strive for Progress not Perfection <br> Portfolio in Progress !!</p>
+<div class="social-media">
+    <a href="#" style="--i:7"><i class='bx bxl-facebook'></i></a>
+    <a href="#" style="--i:8"><i class='bx bxl-instagram-alt' ></i></a>
+    <a href="#" style="--i:9"><i class='bx bxl-linkedin' ></i></a>
+    <a href="#" style="--i:10"><i class='bx bxl-github' ></i></a>
+</div>
+<a href="#" class="btn">Download CV</a>
+</div> 
+    <div class="home-img">
+        <img src="aa/aa.jpg" alt="" >
+    </div>
+
+    </section>
+    <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+strings:["Competetive Coder","Frontend Developer","Software Engineer"],
+typespeed:100,
+backspeed:100,
+backdelay:100,
+loop:true
+})
